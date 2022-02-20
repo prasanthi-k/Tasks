2fca697aa7f49d46dc591baa28709633ee784fa6
prasanthi-k <46198559+prasanthi-k@users.noreply.github.com>
2022-02-20 20:00:48 +0530

    Update and rename FourBalls.java to FourBalls.js
    procedural attempt for FourBalls game using java script

---
+++ FourBalls.js
@@ -0,0 +1,13 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+}
+ var x=1;
+function draw() {
+	ellipse(x,windowHeight/5, 20, 20);
+	ellipse(x*2,2*windowHeight/5, 20, 20);
+	ellipse(x*3,3*windowHeight/5, 20, 20);
+	ellipse(x*4,4*windowHeight/5, 20, 20);
+	x++;
+}
+
